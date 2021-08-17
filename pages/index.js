922b7e7f74c4d5851a43a09a8142ebbe461eba14
FourSqweez <four7518@gmail.com>
2021-08-17 17:00:02 +0700

    Add : Get data from API and send to MediumCard cpn

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import SmallCard from '../components/SmallCard'
+import MediumCard from './../components/MediumCard'
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardData }) {
 	return (
 		<div>
 			<Head>
@@ -31,10 +32,15 @@ export default function Home({ exploreData }) {
 					</div>
 				</section>
 
-				<section className="pt-6">
+				<section className="pt-6 ">
 					<h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+					<div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+						{cardData?.map(({ img, title }) => (
+							<MediumCard key={img} img={img} title={title} />
+						))}{' '}
+					</div>
 				</section>
-				
 			</main>
 		</div>
 	)
@@ -45,9 +51,14 @@ export async function getStaticProps() {
 		(res) => res.json()
 	)
 
+	const cardData = await fetch('https://links.papareact.com/zp1').then((res) =>
+		res.json()
+	)
+
 	return {
 		props: {
 			exploreData,
+			cardData,
 		},
 	}
 }
