2db0bb1ddabab74a3834e7d5227b8e63ac3c53ba
FourSqweez <four7518@gmail.com>
2021-08-27 19:26:25 +0700

    refactor code

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
-import  Router  from 'next/router'
+import Router from 'next/router'
 import ProgressBar from '@badrap/bar-of-progress'
-
 
 const progress = new ProgressBar({
 	size: 4,
 	color: '#fe595e',
 	className: 'z-50',
-	delay: 100
+	delay: 100,
 })
 
 Router.events.on('routeChangeStart', progress.start)
