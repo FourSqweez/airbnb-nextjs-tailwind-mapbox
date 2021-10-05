1ec1e3f73337e73ae315f291b31b2e819cc5eaf8
FourSqweez <four7518@gmail.com>
2021-10-05 15:00:32 +0700

    Refactor code

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
 	images: {
 		domains: ['links.papareact.com'],
 	},
+	env: {
+		mapbox_key:
+			'pk.eyJ1IjoiZm91cnNxd2VleiIsImEiOiJja3Q5c25zcmwxZmJuMnZzMWg4N3doeGlkIn0.vF09HG-sGTA6H6yGcdM9Ig',
+	},
 }
